aec82141c684f843dbf9d7b42453ad106d522a99
ilhame <ilhamehoumadi15@gmail.com>
2024-04-16 12:10:32 +0100

    costum

--- project/users/admin.py
+++ project/users/admin.py
@@ -4,5 +4,4 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User
 # on ajout ici tous ce qu'on peut afficher dans page administration de django
 
-admin.site.register(User, UserAdmin)
 admin.site.register(Profile)
