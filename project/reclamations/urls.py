2754fa3974a4af5bbacfdeb5c52fc394f04662a8
kawtarsimmad <kawtarsimmad02@gmail.com>
2024-04-16 11:39:49 +0100

    reclamation

--- project/reclamations/urls.py
+++ project/reclamations/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     # Ajoutez vos patterns d'URL ici
+    path('reclamation/',views.reclamation,name='reclamation'),
+    path('reclamations/',views.reclamations,name='reclamations'),
 ]
